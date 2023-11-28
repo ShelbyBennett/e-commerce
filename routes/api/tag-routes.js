c5252dc5156132bb07b45ed7742ef2c35edb0abd
Shelby Bennett <shelshel@Shelbys-MBP.lan>
2023-11-27 19:49:07 -0500

    adding more to routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -18,14 +18,35 @@ Tag.findAll({
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+
 });
 
 router.post('/', (req, res) => {
   // create a new tag
+  try {
+    Tag.create(req.body);
+    res.status(200).json (tagdata);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+ 
+
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+
+  Tag.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  })
+  .then((tag) => {
+    res.status(200).json(tag);
+  }) .catch((err) => {
+    console.log(err);
+    res.status(400).json(err);
+  });
 });
 
 router.delete('/:id', (req, res) => {
